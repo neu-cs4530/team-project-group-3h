0ed0ef36aeb1b4bcf38eb2eda5a53c37badc2425
Michael Briggs <michaelbriggs29@yahoo.com>
2022-03-28 15:19:01 -0400

    Filled in the stubs for ConversationAreasList and PlayersList

--- frontend/src/components/SocialSidebar/PlayersList.tsx
+++ frontend/src/components/SocialSidebar/PlayersList.tsx
@@ -1,5 +1,19 @@
+import { ListItem, OrderedList, Tooltip } from '@chakra-ui/react';
 import React from 'react';
+import Player from '../../classes/Player';
+import useCoveyAppState from '../../hooks/useCoveyAppState';
+import usePlayersInTown from '../../hooks/usePlayersInTown';
+import PlayerName from './PlayerName'
 
+/**
+ * Function that returns a number indicating which numbers comes first alphanumerically
+ * @param a the first string passed in, the base string
+ * @param b the second string passed in, the string being compared to
+ * @returns A positive number if "a" comes before "b", a negative number if "b" comes before "a", and 0 if "a" and "b" are equal
+ */
+export function sortAlphabetically(a: string, b: string): number {
+  return a.localeCompare(b, undefined, {numeric: true});
+}
 
 /**
  * Lists the current players in the town, along with the current town's name and ID
@@ -14,5 +28,24 @@ import React from 'react';
  * 
  */
 export default function PlayersInTownList(): JSX.Element {
-  return <></>
+  const players: Player[] = usePlayersInTown()
+  .map((player: Player) =>(player))
+  .sort((a, b) => sortAlphabetically(a.userName, b.userName));
+  
+  const appState = useCoveyAppState();
+  const listItems = players.map((player) => 
+  (<ListItem key={player.id}> 
+    <PlayerName key={player.id} player={player}/>  
+  </ListItem>));
+
+  return (
+  <div>
+    <Tooltip label={`Town ID: ${appState.currentTownID}`} 
+    aria-label={`Town ID: ${appState.currentTownID}`} 
+    role='tooltip'>
+      <h2> <strong>{`Current town: ${appState.currentTownFriendlyName}`}</strong></h2>
+    </Tooltip>
+    <OrderedList> {listItems} </OrderedList>
+  </div>
+ );
 }
