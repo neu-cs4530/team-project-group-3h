dd5c3a20655139edbb5a8c249d0206eec7161466
Abby Cossette <abbycossette@gmail.com>
2022-04-01 12:34:51 -0400

    Changed mock lobby to use state

--- frontend/src/components/Game/WordleLobby.tsx
+++ frontend/src/components/Game/WordleLobby.tsx
@@ -1,59 +1,6 @@
 import { Box, Button, Stack, VStack, Text } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import useCoveyAppState from '../../hooks/useCoveyAppState';
-
-function redButtonText() {
-  const myPlayerID = 5;
-  // will need to create a hook that gets the state of the model, so that we can loop through the list of players
-  // to find what state this player is
-  const redPlayers = [1, 3, 5];
-
-  if(redPlayers.includes(myPlayerID)) {
-    return "Leave red team";
-  }
-  return "Join red team";
-}
-
-function blueButtonText() {
-  const myPlayerID = 5;
-  // will need to create a hook that gets the state of the model, so that we can loop through the list of players
-  // to find what state this player is
-  const bluePlayers = [2, 4, 6];
-
-  if(bluePlayers.includes(myPlayerID)) {
-    return "Leave blue team";
-  }
-  return "Join blue team";
-}
-
-function yourTeamHeader() {
-  // const { myPlayerID } = useCoveyAppState();
-  const myPlayerID = 5;
-  // will need to create a hook that gets the state of the model, so that we can loop through the list of players
-  // to find what state this player is
-  const redPlayers = [1, 3, 5];
-  const bluePlayers = [2, 4, 6];
-  
-  if(redPlayers.includes(myPlayerID)) {
-    return "You are Red!";
-  }
-  if(bluePlayers.includes(myPlayerID)) {
-    return "You are Blue!";
-  }
-  return "You are Spectating!"; 
-}
-
-function mockRedClick() {
-  console.log("Red button was clicked");
-}
-
-function mockBlueClick() {
-  console.log("Blue button was clicked");
-}
-
-function mockStartGameClick() {
-  console.log("Start game button was clicked");
-}
 
 /**
  * Should show players in the conversation area (if we have this as a side bar next to a conversation area we don't need to replicate this), 
@@ -63,20 +10,27 @@ function mockStartGameClick() {
  * (we could integrate it on screen later if needed)
  *
  */
- export default function WordleLobby(): JSX.Element {
-    return (
-      <VStack align='center'>
+export default function WordleLobby(): JSX.Element {
+  const [team, setTeam] = useState('none');
+  
+  const yourTeamHeader = (team === 'none') ? 'You are Spectating!' : `You are ${  team  }!`;
+  const redButtonText = (team === 'red') ? 'Leave Red Team' : 'Join Red Team';
+  const blueButtonText = (team === 'blue') ? 'Leave Blue Team' : 'Join Blue Team';
+
+  return (
+    <VStack align='center'>
+      <Text fontSize='lg'>Join a team to play Wordle!</Text>
       <Stack spacing={2} direction='row' align='center'>
-        <Button onclick={mockRedClick} colorScheme='red' size='sm'>{redButtonText()}</Button>
-        <Button onclick={mockBlueClick} colorScheme='blue' size='sm'>{blueButtonText()}</Button>
+        <Button onclick={() => (team === 'red') ? setTeam('none') : setTeam('red')} colorScheme='red' size='sm'>{redButtonText}</Button>
+        <Button onclick={() => (team === 'blue') ? setTeam('none') : setTeam('blue')} colorScheme='blue' size='sm'>{blueButtonText}</Button>
       </Stack>
-      <Button onclick={mockStartGameClick} colorScheme='green' size='md'>Start Game</Button>
+      <Button colorScheme='green' size='md'>Start Game</Button>
       <Text fontSize='sm'>{yourTeamHeader}</Text>
-      </VStack>
-    );
-  }
+    </VStack>
+  );
+}
 
-  
 
-  
+
+
 
