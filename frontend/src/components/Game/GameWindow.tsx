b88cce1e46357481a3264347eb285bff2a6d3426
Abby Cossette <abbycossette@gmail.com>
2022-04-04 11:09:55 -0400

    Added game list state prior to lobby

--- frontend/src/components/Game/GameWindow.tsx
+++ frontend/src/components/Game/GameWindow.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import { Box } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Box, VStack, Text, Button } from '@chakra-ui/react';
 import WordleLobby from './WordleLobby';
 
-
 export default function GameWindow(): JSX.Element {
-    // will need to use the model hook to access the state of the game
-    
+    // will need to use the model hook to access the state of the game (game list vs lobby vs game play vs game over)
+    const [state, setState] = useState('list');
+
+    if (state === 'list') {
+        return (
+            <VStack align='center'>
+                <Text fontSize='lg'>Choose a game to play!</Text>
+                <Button onClick={() => setState('lobby')} colorScheme='blue' size='md'>Wordle</Button>
+                <Button onClick={() => window.open("https://sudoku.com/")} colorScheme='blue' size='md'>Sudoku</Button>
+                <Button onClick={() => window.open("https://www.washingtonpost.com/crossword-puzzles/daily/")} colorScheme='blue' size='md'>Daily Crossword</Button>
+                <Button onClick={() => window.open("https://playtictactoe.org/")} colorScheme='blue' size='md'>Tic Tac Toe</Button>
+            </VStack>
+        );
+    }
     return <Box><WordleLobby /></Box>;
 };
