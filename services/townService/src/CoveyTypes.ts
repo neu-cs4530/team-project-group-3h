5466251b4e7b1d0de5442e764eede784f4c6b382
Mobolaji Rotibi <brotibi@gmail.com>
2022-04-18 19:27:53 -0400

    Wowie hopefully this isn't broken

--- services/townService/src/CoveyTypes.ts
+++ services/townService/src/CoveyTypes.ts
@@ -22,15 +22,9 @@ export type GameAction = {
 };
 
 export type GameState = {
-<<<<<<< HEAD
-  teamOneState: TeamState;
-  teamTwoState: TeamState;
-  // teams: Map<String, TeamState>;
-=======
   teamOneState: TeamState|undefined;
   teamTwoState: TeamState|undefined;
   //teams: Map<String, TeamState>;
->>>>>>> ad4a87b (Please don't touch any of these files)
   winner: string;
   isActive: boolean;
 };
